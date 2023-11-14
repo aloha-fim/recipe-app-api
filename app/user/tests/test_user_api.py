1ab929e8c536ba960b329d23ecc4d11b5dbff562
aloha-fim <frederec.im@gmail.com>
2023-11-14 17:29:13 +0800

    fix test create user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -42,12 +42,12 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
-            'name': 'Test Name'
+            'name': 'Test Name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUESTS)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars"""
