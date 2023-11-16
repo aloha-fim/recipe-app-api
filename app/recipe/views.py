ce63bfc00bf50a1f70cd009289ea8521b06c8c4c
aloha-fim <frederec.im@gmail.com>
2023-11-16 23:17:38 +0800

    pass test to tag listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,11 +38,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModeMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Filter queryset to authenticated user"""
